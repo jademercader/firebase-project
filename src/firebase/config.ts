e6eec84785a77eed4d05383b72121e44811eef6c
Jade Mercader <jademercader333@gmail.com>
2026-03-02 04:42:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4208818908-a8dd1",
+  "appId": "1:380533084186:web:f69d28f321e2d4fc8c01a3",
+  "apiKey": "AIzaSyBK8WsTcTzst_dLURtCJQAH4odQmKl44MM",
+  "authDomain": "studio-4208818908-a8dd1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "380533084186"
+};
